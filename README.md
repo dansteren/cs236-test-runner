a555a3b2f6b0509c3f362223a46c2a7d25248275
Dan Steren <daniel.d.steren@gmail.com>
2016-02-12 00:39:40 -0700

    Added man page and test case downloading Man page has been moved from ./test.sh to ./manpage. Downloading test cases from the course website has been added with the dependency on a cookies.txt file.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Test.sh is a comprehensive test runner. It can test your project against a singl
 
 ## Get Started
 
-### Setup your environment
+### Prep your Project
 
 Test.sh has been designed to work with the following folder structure:
 
@@ -34,21 +34,40 @@ Test.sh has been designed to work with the following folder structure:
 
 In order to use the script you will either need to setup your folder structure to match, or you will need to modify the script to point to the correct locations (step 4 below).
 
+### Get the Test Runner
+
 Once you have the correct file structure, do the following:
 
 1. Clone the repo: `git clone https://github.com/xaves/cs236-test-runner.git`
 2. Copy the file into the root of your project directory.
 3. Make the file executable for your user: `chmod u+x test.sh`
 4. Update the output paths (if necessary): `./test.sh -u`
-5. Run the script with any of the available options. `./test.sh <options>`
+5. [BETA!] Download the test files: `./test.sh -d`
+6. Run the script with any of the available options. `./test.sh <options>`
 
 
-### Options
+## Options
 
- - `-u`     update output paths
- - `-c`     compile the program before running tests
- - `-f`     only test the file passed in
- - `-v`     output version information
- - `-h`     display help
+Test.sh can be run with various different options that provide more functionality
+
+    -u     update output paths
+    -c     compile the program before running tests
+    -d     download all test files from the course website [BETA!]
+    -f     only test the file passed in
+    -v     output version information
+    -h     display help
 
 Example: `./test.sh -cf 21`
+
+## Getting Test files
+
+Because students are required to come up with their own test cases all test cases will have to be download separately from the [course website](http://beta.cs.byu.edu/~cs236/tests/tests.php).
+
+### [BETA FEATURE!] Download test cases with the script
+
+An experimental feature has been added on the beta branch. It allows you to download all the test cases from the course website. You must have Firefox installed in order to use this feature. Future releases may eliminate the need for Firefox. To use, follow these steps:
+1. Pull the latest changes from GitHub `git pull`
+2. Checkout the beta branch `git checkout beta`
+3. Install the [Export Cookies](https://addons.mozilla.org/en-US/firefox/addon/export-cookies/) Firefox add-on
+4. Inside Firefox go to Tools > Export Cookies and save the cookies.txt file to the same location as test.sh
+5. Run `./test.sh -d`
--- test.sh
+++ test.sh
@@ -6,33 +6,12 @@ EXPECT=test/$LAB/expect/
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
 WHITE='\033[0m'
 
 case $1 in
   -h|--help)
-    echo "TEST(7)                             test.sh                            TEST(7)"
-    echo ""
-    echo "NAME"
-    echo "       test.sh - test your project against a series of test cases"
-    echo ""
-    echo "SYNOPSIS"
-    echo "       ./test.sh [OPTION]... [FILE]"
-    echo ""
-    echo "DESCRIPTION"
-    echo "       test.sh is a comprehensive test runner. It can test your project"
-    echo "       against a single test case or a whole folder, with the additional"
-    echo "       option to compile your program before testing."
-    echo ""
-    echo "OPTIONS"
-    echo "       -u     update output paths"
-    echo ""
-    echo "       -c     compile the program before running tests"
-    echo ""
-    echo "       -f     only test the file passed in"
-    echo ""
-    echo "       -v     output version information and exit"
-    echo ""
-    echo "       -h     display this help and exit"
+    man ./manpage
   ;;
   -u|--update)
     echo "Updating File Paths (leave field empty to skip)"
@@ -69,8 +48,116 @@ case $1 in
     fi
     printf "${GREEN}    UPDATE COMPLETE!${WHITE}\n"
   ;;
+  -d|--download-tests)
+    printf "Downloading Test Cases... ["
+
+    #see http://askubuntu.com/questions/161778/how-do-i-use-wget-curl-to-download-from-a-site-i-am-logged-into
+
+    # lab-1 test cases
+    LAB="lab-1"
+    INPUT=test/$LAB/input/
+    EXPECT=test/$LAB/expect/
+        # input
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab1/in10.txt --create-dirs -o $INPUT/10
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab1/in11.txt --create-dirs -o $INPUT/11
+            echo -n "="
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab1/in12.txt --create-dirs -o $INPUT/12
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab1/in13.txt --create-dirs -o $INPUT/13
+            echo -n "="
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab1/in14.txt --create-dirs -o $INPUT/14
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab1/in15.txt --create-dirs -o $INPUT/15
+            echo -n "="
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab1/in61.txt --create-dirs -o $INPUT/61
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab1/in62.txt --create-dirs -o $INPUT/62
+            echo -n "="
+        # expect
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab1/out10.txt --create-dirs -o $EXPECT/10
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab1/out11.txt --create-dirs -o $EXPECT/11
+            echo -n "="
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab1/out12.txt --create-dirs -o $EXPECT/12
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab1/out13.txt --create-dirs -o $EXPECT/13
+            echo -n "="
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab1/out14.txt --create-dirs -o $EXPECT/14
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab1/out15.txt --create-dirs -o $EXPECT/15
+            echo -n "="
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab1/out61.txt --create-dirs -o $EXPECT/61
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab1/out62.txt --create-dirs -o $EXPECT/62
+            echo -n "="
+    # lab-2 test cases
+    LAB="lab-2"
+    INPUT=test/$LAB/input/
+    EXPECT=test/$LAB/expect/
+        # input
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/in21.txt --create-dirs -o $INPUT/21
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/in22.txt --create-dirs -o $INPUT/22
+            echo -n "="
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/in23.txt --create-dirs -o $INPUT/23
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/in24.txt --create-dirs -o $INPUT/24
+            echo -n "="
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/in25.txt --create-dirs -o $INPUT/25
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/in25.txt --create-dirs -o $INPUT/25
+            echo -n "="
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/in25.txt --create-dirs -o $INPUT/25
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/in25.txt --create-dirs -o $INPUT/25
+            echo -n "="
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/in61.txt --create-dirs -o $INPUT/61
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/in62.txt --create-dirs -o $INPUT/62
+            echo -n "="
+        # expect
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/out21.txt --create-dirs -o $EXPECT/21
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/out22.txt --create-dirs -o $EXPECT/22
+            echo -n "="
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/out23.txt --create-dirs -o $EXPECT/23
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/out24.txt --create-dirs -o $EXPECT/24
+            echo -n "="
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/out25.txt --create-dirs -o $EXPECT/25
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/out25.txt --create-dirs -o $EXPECT/25
+            echo -n "="
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/out25.txt --create-dirs -o $EXPECT/25
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/out25.txt --create-dirs -o $EXPECT/25
+            echo -n "="
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/out61.txt --create-dirs -o $EXPECT/61
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab2/out62.txt --create-dirs -o $EXPECT/62
+            echo -n "="
+    # lab-3 test cases
+    LAB="lab-3"
+    INPUT=test/$LAB/input/
+    EXPECT=test/$LAB/expect/
+        # input
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab3/in30.txt --create-dirs -o $INPUT/30
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab3/in33.txt --create-dirs -o $INPUT/33
+            echo -n "="
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab3/in35.txt --create-dirs -o $INPUT/35
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab3/in36.txt --create-dirs -o $INPUT/36
+            echo -n "="
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab3/in37.txt --create-dirs -o $INPUT/37
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab3/in61.txt --create-dirs -o $INPUT/61
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab3/in62.txt --create-dirs -o $INPUT/62
+            echo -n "="
+        # expect
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab3/out30.txt --create-dirs -o $EXPECT/30
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab3/out33.txt --create-dirs -o $EXPECT/33
+            echo -n "="
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab3/out35.txt --create-dirs -o $EXPECT/35
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab3/out36.txt --create-dirs -o $EXPECT/36
+            echo -n "="
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab3/out37.txt --create-dirs -o $EXPECT/37
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab3/out61.txt --create-dirs -o $EXPECT/61
+            curl -s --cookie cookies.txt http://beta.cs.byu.edu/~cs236/tests/lab3/out62.txt --create-dirs -o $EXPECT/62
+            echo -n "="
+    # lab-4 test cases
+        # input
+        # expect
+    # lab-5 test cases
+        # input
+        # expect
+    LAB="lab-2"
+    INPUT=test/$LAB/input/
+    EXPECT=test/$LAB/expect/
+    printf "]\n${GREEN}All Test Cases Downloaded!${WHITE}\n"
+  ;;
   -v|--version)
-    echo "0.1.1"
+    echo "0.1.2"
   ;;
   -cf|-fc|-f)
     if [ "$1" == "-cf" ]||[ "$1" == "-fc" ]
